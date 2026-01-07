1f3998fe2635fca4584694cfd4be262c04c93094
Funke Sowole <funkegoodvibe@gmail.com>
2026-01-07 00:46:51 +0000

    add comments

--- 3_project_mngt_1_top_2_products copy.sql
+++ 3_project_mngt_1_top_2_products copy.sql
@@ -35,6 +35,15 @@ Answer the following questions.
 --where ongoing projects have project.end_date set to NULL. 
 --Output columns: product_id, active_projects_count
 
+'''
+Step 1) Join product, with project_assigment, and project tables - using an inner join. These ensure we have access to the appropriate columns. 
+Step 2) Add the condition where end date IS NULL, to ensure we look for live projects only. 
+Step 3) COUNT the number of project ids, as <active project count>, and output the product id. 
+Step 4) Aggregate - on the product ID 
+Step 5) ORDER BY, and limit to output the top 2. 
+
+
+'''
 SELECT p.product_id, COUNT(prj.project_id) AS active_projects_count
 FROM product p
 INNER JOIN project_assignment pa ON p.product_id = pa.product_id
